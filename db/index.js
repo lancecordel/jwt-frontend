fd8261718587a30b682f78e377d4752ec8f65f3c
Lance Wiltshire <lancecordel@gmail.com>
2022-03-11 10:17:50 -0500

    update

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-// let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb+srv://Lance:2A0BStlAwb07Qb3L@cluster0.vrugn.mongodb.net/people?retryWrites=true&w=majority';
+let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb+srv://Lance:2A0BStlAwb07Qb3L@cluster0.vrugn.mongodb.net/people?retryWrites=true&w=majority';
 
 mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
     console.log('Successfully connected to MongoDB')
